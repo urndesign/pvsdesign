07added747c3cb1ad5361de75bedc2f2cbe66e4a
urndesign <urndesign@gmail.com>
2015-10-08 14:10:02 +0300

    Создание index.php

---
+++ index.php
@@ -0,0 +1,111 @@
+<?php
+
+//require_once('database.php');
+//require_once('models/articles.php');
+
+//$articles = articles_all();
+echo "index.php";
+?>
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>pvsdesign</title>
+    <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+  </head>
+  <body>
+    <header>
+      
+    </header>
+    
+    <article>
+      
+    </article>
+    
+    <footer>
+      <p>Copypight &copy; 2015 PVSdesign</p>
+    </footer>
+  </body>
+</html>
+
+
+
+<!--// Клиент-серверные приложения 2:20:00
+http://geekbrains.ru/events/108
+
+// Правила хорошего тона для верстальщика 0:56:00
+http://geekbrains.ru/events/102
+
+// Секреты интервью от разработчика Skype, Intel, Microsoft и Huawei 1:31:00
+http://geekbrains.ru/events/93
+
+// Оптимизируем сайт для продвижения 1:59:00
+http://geekbrains.ru/events/97
+
+// Оформление элементов HTML при помощи CSS 1:20:00
+http://geekbrains.ru/events/99
+
+// Верстка сайтов, с чего начать? 1:59:00
+http://geekbrains.ru/events/96
+
+// Тестирование на проникновение: легальный хакинг 1:17:00
+http://geekbrains.ru/events/82
+
+// Оживляем веб с помощью JQuery, AJAX, JSON, JSONP и iframe 2:43:00
+http://geekbrains.ru/events/77
+
+// Веб-сервер по-взрослому 2:30:00
+http://geekbrains.ru/events/71
+
+// Облачный хостинг как тестовая площадка для разработчика и админа 1:48:00
+http://geekbrains.ru/events/78
+
+// Популярные сетевые протоколы и форматы обмена данными 2:21:00
+http://geekbrains.ru/events/59
+
+// Зачем программисту нужны алгоритмы и как устроиться на работу в Google 1:14:00
+http://geekbrains.ru/events/57
+
+// Эффективный поиск информации в интернете 1:50:00
+http://geekbrains.ru/events/56
+
+// Сайт как "раз, два, три!" с помощью yii фреймворка 1:13:00
+http://geekbrains.ru/events/51
+
+// Обзор популярных библиотек и фреймворков для PHP 2:08:00
+http://geekbrains.ru/events/44
+
+// Карта развития веб-программиста 2:25:00
+http://geekbrains.ru/events/34
+
+// HTML/CSS, JavaScript и Ruby/Rails. Инструменты на все случаи жизни 0:54:00
+http://geekbrains.ru/events/30
+
+// Зачем фреймворки веб-разработчику 1:34:00
+http://geekbrains.ru/events/25
+
+// Зачем Вам изучать Linux 1:07:00
+http://geekbrains.ru/events/26
+
+// 1С Битрикс - система для успешной веб-разработки 1:01:00
+http://geekbrains.ru/events/20
+
+// Битрикс24. Повышаем эффективность работы 0:48:00
+http://geekbrains.ru/events/18
+
+// Cоздание браузерной игры в прямом эфире на HTML/CSS/JS 3:16:00
+http://geekbrains.ru/events/1
+
+// "Магия" и другие неочевидные моменты и "секреты" в PHP 1:41:00
+http://geekbrains.ru/events/14
+
+// Создание парсера сайтов на языке Python 1:28:00
+http://geekbrains.ru/events/9
+
+// Обзорный вебинар по миру Ruby и Rails 1:10:00
+http://geekbrains.ru/events/4
+
+// Обзор современных технологий эффективной веб-разработки 1:01:00
+http://geekbrains.ru/events/3
+-->
